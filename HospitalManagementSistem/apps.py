04c1be2ed8a941bd3ebf50aa499201022ec89cbb
pepe <211078@std.hi-joho.ac.jp>
2024-05-20 10:58:35 +0900

    HospitalManagementSistem

---
+++ HospitalManagementSistem/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HospitalmanagementsistemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HospitalManagementSistem'
